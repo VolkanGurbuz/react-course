d7de6ba5d661f433929889a86b646bbb666878c6
volkangurbuz <volkangurbuz16@gmail.com>
2021-11-14 19:31:28 +0100

    created todo component

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//first execute code will be run on the html we load on our project
+// we want to render our app html element our custom element which in app.js file into this place of this element root id
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App/>, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
