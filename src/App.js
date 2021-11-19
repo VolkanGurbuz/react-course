18a5c0ea12807c78d540fa6afc4754da24ae1210
volkangurbuz <volkangurbuz16@gmail.com>
2021-11-19 22:45:15 +0100

    set user input, handled submit data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import { Route, Switch } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
 import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
+
 
 function App() {
     return (
-        <div>
-            <MainNavigation/>
+        <Layout>
             <Switch>
                 <Route path='/' exact>
-                    <AllMeetupsPage />
+                    <AllMeetupsPage/>
                 </Route>
                 <Route path='/new-meetup'>
-                    <NewMeetupPage />
+                    <NewMeetupPage/>
                 </Route>
                 <Route path='/favorites'>
-                    <FavoritesPage />
+                    <FavoritesPage/>
                 </Route>
             </Switch>
-        </div>
+        </Layout>
+
     );
 }
+
 export default App;
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,11 @@
-function NewMeetupPage(){
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
-    return <div>NewMeetupPage</div>;
+function NewMeetupPage() {
+
+    return <section>
+        <h1>Meet up new</h1>
+        <NewMeetupForm/>
+    </section>;
 
 }
 
